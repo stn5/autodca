1d8930b17b1325e2cfd5708b680bba68223436ab
Stanislav <stnndw@gmail.com>
2026-05-20 06:27:15 +0300

    initial project structure

---
+++ test/AutoDCATest.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {AutoDCA} from "../src/AutoDCA.sol";
+
+contract TestAutoDCA is Test {
+    
+}
