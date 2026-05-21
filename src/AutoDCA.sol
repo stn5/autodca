d6f2e2462a71788a85818d47401cf7082e627358
Stanislav <stnndw@gmail.com>
2026-05-21 04:15:28 +0300

    add deposit function and tests

--- src/AutoDCA.sol
+++ src/AutoDCA.sol
@@ -1,6 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract AutoDCA {
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+contract AutoDCA {
+    using SafeERC20 for IERC20;
+
+    address public immutable I_USDC;
+    uint256 public constant MINIMUM_DEPOSIT = 50e6;
+    mapping(address => uint256) public userBalances;
+
+    constructor(address usdc) {
+        I_USDC = usdc;
+    }
+
+    function deposit(uint256 amount) external {
+        require(amount >= MINIMUM_DEPOSIT, "Minimum deposit is 50 USDC");
+        IERC20(I_USDC).safeTransferFrom(msg.sender, address(this), amount);
+        userBalances[msg.sender] += amount;
+    }
 }
--- test/AutoDCATest.t.sol
+++ test/AutoDCATest.t.sol
@@ -3,7 +3,38 @@ pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 import {AutoDCA} from "../src/AutoDCA.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TestAutoDCA is Test {
-    
+    AutoDCA autoDca;
+    address user = makeAddr("user");
+
+    function setUp() external {
+        autoDca = new AutoDCA(0x0000000000000000000000000000000000000000);
+    }
+
+    function testMinimumDepositIsFifty() public view {
+        assertEq(autoDca.MINIMUM_DEPOSIT(), 50e6);
+    }
+
+    function testDepositSuccess() public {
+        uint256 amount = 70e6;
+        
+        vm.mockCall(
+            autoDca.I_USDC(),
+            abi.encodeCall(IERC20.transferFrom, (user, address(autoDca), amount)),
+            abi.encode(true)
+        );
+        
+        vm.prank(user);
+        autoDca.deposit(amount);
+
+        assertEq(autoDca.userBalances(user), amount);
+    }
+
+    function testDepositRevertsIfAmountBelowMinimum() public {
+        vm.prank(user);
+        vm.expectRevert("Minimum deposit is 50 USDC");
+        autoDca.deposit(40e6);
+    }
 }
