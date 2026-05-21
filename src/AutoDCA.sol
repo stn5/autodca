30108476d2e2220b0855f2f0e4b06ab9f9bf717c
Stanislav <stnndw@gmail.com>
2026-05-21 06:47:46 +0300

    add withdraw function and tests

--- src/AutoDCA.sol
+++ src/AutoDCA.sol
@@ -7,13 +7,19 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 contract AutoDCA {
     using SafeERC20 for IERC20;
 
+    /* Errors */
+    error AutoDCA__InvalidTokenAddress();
     error AutoDCA__AmountBelowMinimum();
+    error AutoDCA__AmountMustBeGreaterThanZero();
+    error AutoDCA__InsufficientBalance();
 
     address public immutable I_USDC;
     uint256 public constant MINIMUM_DEPOSIT = 50e6;
     mapping(address => uint256) public userBalances;
 
+    /* Events */
     event Deposited(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
 
     constructor(address usdc) {
         I_USDC = usdc;
@@ -24,5 +30,13 @@ contract AutoDCA {
         IERC20(I_USDC).safeTransferFrom(msg.sender, address(this), amount);
         userBalances[msg.sender] += amount;
         emit Deposited(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount) external {
+        if (amount == 0) revert AutoDCA__AmountMustBeGreaterThanZero();
+        if (userBalances[msg.sender] < amount) revert AutoDCA__InsufficientBalance();
+        userBalances[msg.sender] -= amount;
+        IERC20(I_USDC).safeTransfer(msg.sender, amount);
+        emit Withdrawn(msg.sender, amount);
     }
 }
--- test/AutoDCATest.t.sol
+++ test/AutoDCATest.t.sol
@@ -3,14 +3,20 @@ pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 import {AutoDCA} from "../src/AutoDCA.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {MockUSDC} from "./mocks/MockUSDC.sol";
 
 contract TestAutoDCA is Test {
     AutoDCA autoDca;
+    MockUSDC usdc;
     address user = makeAddr("user");
 
+    uint256 constant USER_STARTING_BALANCE = 500e6;
+    uint256 constant DEPOSIT_AMOUNT = 70e6;
+
     function setUp() external {
-        autoDca = new AutoDCA(makeAddr("usdc"));
+        usdc = new MockUSDC();
+        autoDca = new AutoDCA(address(usdc));
+        usdc.mint(user, USER_STARTING_BALANCE);
     }
 
     function testMinimumDepositIsFifty() public view {
@@ -18,23 +24,56 @@ contract TestAutoDCA is Test {
     }
 
     function testDepositSuccess() public {
-        uint256 amount = 70e6;
-        
-        vm.mockCall(
-            autoDca.I_USDC(),
-            abi.encodeCall(IERC20.transferFrom, (user, address(autoDca), amount)),
-            abi.encode(true)
-        );
-        
-        vm.prank(user);
-        autoDca.deposit(amount);
+        vm.startPrank(user);
 
-        assertEq(autoDca.userBalances(user), amount);
+        usdc.approve(address(autoDca), DEPOSIT_AMOUNT);
+        autoDca.deposit(DEPOSIT_AMOUNT);
+
+        vm.stopPrank();
+
+        assertEq(autoDca.userBalances(user), DEPOSIT_AMOUNT);
+        assertEq(usdc.balanceOf(user), USER_STARTING_BALANCE - DEPOSIT_AMOUNT);
+        assertEq(usdc.balanceOf(address(autoDca)), DEPOSIT_AMOUNT);
     }
 
     function testDepositRevertsIfAmountBelowMinimum() public {
+        uint256 amount = autoDca.MINIMUM_DEPOSIT() - 1;
         vm.prank(user);
         vm.expectRevert(AutoDCA.AutoDCA__AmountBelowMinimum.selector);
-        autoDca.deposit(40e6);
+        autoDca.deposit(amount);
+    }
+
+    function testWithdrawSuccess() public {
+        uint256 withdrawAmount = 30e6;
+
+        vm.startPrank(user);
+
+        usdc.approve(address(autoDca), DEPOSIT_AMOUNT);
+        autoDca.deposit(DEPOSIT_AMOUNT);
+        autoDca.withdraw(withdrawAmount);
+
+        vm.stopPrank();
+
+        assertEq(autoDca.userBalances(user), DEPOSIT_AMOUNT - withdrawAmount);
+        assertEq(usdc.balanceOf(user), USER_STARTING_BALANCE - DEPOSIT_AMOUNT + withdrawAmount);
+        assertEq(usdc.balanceOf(address(autoDca)), DEPOSIT_AMOUNT - withdrawAmount);
+    }
+
+    function testWithdrawRevertsIfAmountIsZero() public {
+        vm.prank(user);
+        vm.expectRevert(AutoDCA.AutoDCA__AmountMustBeGreaterThanZero.selector);
+        autoDca.withdraw(0);
+    }
+
+    function testWithdrawRevertsIfInsufficientBalance() public {
+        vm.startPrank(user);
+
+        usdc.approve(address(autoDca), DEPOSIT_AMOUNT);
+        autoDca.deposit(DEPOSIT_AMOUNT);
+
+        vm.expectRevert(AutoDCA.AutoDCA__InsufficientBalance.selector);
+        autoDca.withdraw(DEPOSIT_AMOUNT + 1);
+
+        vm.stopPrank();
     }
 }
