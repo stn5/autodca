735e25dcd17a3e29f34dfe08af6dc363ed066b1c
Stanislav <stnndw@gmail.com>
2026-05-25 06:17:33 +0300

    add Deploy script

--- script/AutoDCADeploy.s.sol
+++ script/AutoDCADeploy.s.sol
@@ -3,7 +3,21 @@ pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
 import {AutoDCA} from "../src/AutoDCA.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployAutoDCA is Script {
-    
+    function run() public {
+        deployContract();
+    }
+
+    function deployContract() public returns (AutoDCA, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory networkConfig = helperConfig.getConfig();
+
+        vm.startBroadcast();
+        AutoDCA autoDca = new AutoDCA(networkConfig.usdc);
+        vm.stopBroadcast();
+
+        return (autoDca, helperConfig);
+    }
 }
---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {MockUSDC} from "../test/mocks/MockUSDC.sol";
+
+abstract contract CodeConstants {
+    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
+    uint256 public constant BASE_MAINNET_CHAIN_ID = 8453;
+    uint256 public constant LOCAL_CHAIN_ID = 31337;
+}
+
+contract HelperConfig is CodeConstants, Script {
+    error HelperConfig__InvalidChainId();
+
+    struct NetworkConfig {
+        address usdc;
+    }
+
+    NetworkConfig public localNetworkConfig;
+    mapping(uint256 => NetworkConfig) public networkConfigs;
+
+    constructor() {
+        networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaConfig();
+        networkConfigs[BASE_MAINNET_CHAIN_ID] = getBaseConfig();
+    }
+
+    function getConfig() public returns (NetworkConfig memory) {
+        return getConfigByChainId(block.chainid);
+    }
+
+    function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory) {
+        if (networkConfigs[chainId].usdc != address(0)) {
+            return networkConfigs[chainId];
+        } else if (chainId == LOCAL_CHAIN_ID) {
+            return getOrCreateAnvilConfig();
+        } else {
+            revert HelperConfig__InvalidChainId();
+        }
+    }
+
+    function getSepoliaConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({
+            usdc: 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238
+        });
+    }
+
+    function getBaseConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({
+            usdc: 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913
+        });
+    }
+
+    function getOrCreateAnvilConfig() public returns (NetworkConfig memory) {
+        if (localNetworkConfig.usdc != address(0)) {
+            return localNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockUSDC mockUsdc = new MockUSDC();
+        vm.stopBroadcast();
+
+        localNetworkConfig = NetworkConfig({usdc: address(mockUsdc)});
+        return localNetworkConfig;
+    }
+}
